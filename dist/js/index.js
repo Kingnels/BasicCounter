e4175ad56b8c84c7a12cec7c81fdf943a940016f
Kingnels <ifeanyiibe2022@gmail.com>
2022-09-21 18:44:59 +0100

    commit

--- dist/js/index.js
+++ dist/js/index.js
@@ -17,13 +17,14 @@ btns.forEach((x)=>{
         }
         value.textContent = count
 
-        if(styles.contains('decrease')){
-            value.style.color = "red"
-        }
-        else if(styles.contains('increase')){
+        if(count>0){
             value.style.color = "green"
         }
+        else if(count < 0){
+            value.style.color = "red"
+        }
         else{
+            count == 0
             value.style.color = "black"
         }
     })
